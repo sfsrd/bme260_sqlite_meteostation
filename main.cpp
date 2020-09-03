25e1ba19cdb7e888404992f2f46b0e52ad480ff0
Sophie <49619115+sfsrd@users.noreply.github.com>
2020-09-03 14:27:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 #include <stdint.h>
 #include <zconf.h>
 #include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <cstring>
 #include "bme280.h"
 #include "i2c_driver.h"
 #include "sqlite3.h"
-#include <ctime>
-#include <iomanip>
+#include "date.h"
+#include "database.h"
+
+using namespace std::chrono;
 
 int i2c_init();
 int i2c_read(uint8_t reg, int read_len, uint8_t read_res[]);
@@ -31,72 +39,64 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
     return 0;
 }
 
+std::string getCurrentDate(){
+    std::string currentDate;
+    currentDate = date::format("%F %T", time_point_cast<milliseconds>(system_clock::now()));
+    return currentDate;
+}
+
 int main() {
     char *device = "/dev/i2c-1";
     int devId = BME280_I2C_ADDRESS;
-    std::string currentTime;
 
-    sqlite3 *db;
-    char *zErrMsg = 0;
+    sqlite3* db;
+    char *tableName = "Info_sensor";
+    char *columns = "id INTEGER PRIMARY KEY AUTOINCREMENT, dt TEXT, temperature REAL, humidity REAL, pressure REAL";
+    char qry[500];
     int rc;
-    char qry[80];
 
-    //TABLE Info (temperature REAL, humidity REAL, pressure REAL)
+    std::string currentDate;
+    char cDate[100];
 
-    char *sqlCreate = "CREATE TABLE Info (temperature REAL, humidity REAL, pressure REAL, date d, time t);";
-    char *sqlDropTable = "DROP TABLE Info";
-    char *sqlShow = "SELECT * FROM Info;";
+    database DB(db, "sensor_data.db");
+    DB.openDB();
+    DB.create_table(qry, tableName, columns);
 
-    rc = sqlite3_open("sensor_data.db", &db);
-    rc = sqlite3_exec(db, sqlShow, callback, 0, &zErrMsg);
-    if( rc != SQLITE_OK ) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    } else {
-        fprintf(stdout, "Operation done successfully\n");
-    }
-    sqlite3_close(db);
+   try {
+         BME280 * bme280 = new BME280(device, devId, i2c_init, i2c_read, i2c_write);
+         int fd = bme280->init();
+         bme280->reset();
 
-    /*try {
-        BME280 * bme280 = new BME280(device, devId, i2c_init, i2c_read, i2c_write);
-        int fd = bme280->init();
-        bme280->reset();
+         if (fd < 0) {
+             printf("Device not found");
+             return -1;
+         }
 
-        if (fd < 0) {
-            printf("Device not found");
-            return -1;
-        }
+         bme280->reset();
+         bme280->setPowerMode(BME280_NORMAL_MODE);
+         bme280->setTemperatureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_TEMPERATURE);
+         bme280->setPressureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_PRESSURE);
+         bme280->setHumidityOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_HUMIDITY);
 
-        bme280->reset();
-        bme280->setPowerMode(BME280_NORMAL_MODE);
-        bme280->setTemperatureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_TEMPERATURE);
-        bme280->setPressureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_PRESSURE);
-        bme280->setHumidityOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_HUMIDITY);
+         while (true) {
+             BMP280Data * bme280Data = bme280->getBMP280Data();
+             printData(*bme280Data);
+             currentDate = getCurrentDate();
+             strcpy(cDate, currentDate.c_str());
+             DB.generateQInsertData(qry, tableName, cDate, bme280Data->getTemperature(),bme280Data->getHumidity(),
+                                    bme280Data->getPressure());
+             std::cout<<"qry = "<< qry <<std::endl;
+             rc = DB.insertQ(qry);
+             std::cout<<"rc = "<< rc <<std::endl;
+             sleep(5);
+         }
 
-        while (true) {
-            rc = sqlite3_open("sensor_data.db", &db);
-            std::cout<<"time&date = "<< currentTime <<std::endl;
-            BMP280Data * bme280Data = bme280->getBMP280Data();
-            printData(*bme280Data);
+     } catch (std::exception & e) {
+         printf("%s\n", e.what());
+     }
 
-            sprintf(qry,"INSERT INTO Info values(%.2f, %.2f, %.2f, date('now'), time('now'));", bme280Data->getTemperature(),bme280Data->getHumidity(),
-                     bme280Data->getPressure());
-            std::cout<<"qry = "<< qry <<std::endl;
-            rc = sqlite3_exec(db, qry, callback, 0, &zErrMsg);
-            if( rc != SQLITE_OK ) {
-                fprintf(stderr, "SQL error: %s\n", zErrMsg);
-                sqlite3_free(zErrMsg);
-            } else {
-                fprintf(stdout, "Operation done successfully\n");
-            }
-            sqlite3_close(db);
-            sleep(5);
-        }
-
-    } catch (std::exception & e) {
-        printf("%s\n", e.what());
-    }*/
-
+    DB.checkOK();
+    DB.closeDB();
     return 0;
 }
 
